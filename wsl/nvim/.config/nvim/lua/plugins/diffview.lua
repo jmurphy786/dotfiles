1f19db99ca057730ef0fd59658d15e0a1956ce03
jordan <jordan.murphy@cirdan.com>
2026-05-14 15:35:29 +0100

    remove alot

--- wsl/nvim/.config/nvim/lua/plugins/diffview.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "sindrets/diffview.nvim",
-    keys = {
-        { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Git file history" },
-        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>",   desc = "Git repo history" },
-        { "<leader>gc", "<cmd>DiffviewClose<cr>",          desc = "Git close" },
-    }
-}
